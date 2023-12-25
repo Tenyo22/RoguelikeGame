25b04c7e21fbc71284733331e493fb21a69787b0
Tenyo22 <ematen@outlook.com>
2023-12-25 17:32:31 -0600

    Moving the square

--- src/ReactRogue.js
+++ src/ReactRogue.js
@@ -1,14 +1,35 @@
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
+import InputManager from "./InputManager.js"
 
 const ReactRogue = ({ width, height, tilesize }) => {
     const canvasRef = useRef(null)
+    let inputManager = new InputManager()
+    const [player, setPlayer] = useState({ x: 64, y: 128 })
+
+    const handleInput = (action, data) => {
+        console.log(`handle action: ${action}:${JSON.stringify(data)}`)
+        let newPlayer = { ...player }
+        newPlayer.x += data.x * tilesize
+        newPlayer.y += data.y * tilesize
+        setPlayer(newPlayer)
+    }
+
+    useEffect(() => {
+        console.log('Bind input')
+        inputManager.bindKeys()
+        inputManager.subscribe(handleInput)
+        return () => {
+            inputManager.unbindKeys()
+            inputManager.unsubscribe(handleInput)
+        }
+    })
 
     useEffect(() => {
         console.log('Draw to canvas')
         const ctx = canvasRef.current.getContext('2d')
         ctx.clearRect(0, 0, width * tilesize, height * tilesize)
         ctx.fillStyle = '#000'
-        ctx.fillRect(12, 22, 16, 16)
+        ctx.fillRect(player.x, player.y, 16, 16)
     })
 
     return (
